e8b1e9e9f450968b3750241bae389e7fd4cc884b
MiK@ <mickael.blondeau@hotmail.fr>
2023-09-02 16:33:07 +0200

    Création objet formation

---
+++ src/app/formation/models/formation.ts
@@ -0,0 +1,11 @@
+export class Formation {
+    public year: string;
+    public title: string;
+    public school: string;
+
+    constructor(year: string, title: string, school: string) {
+        this.year = year;
+        this.title = title;
+        this.school = school;
+    }
+}
